de932d3148db84780c16f3198a8b5e768b65fdb6
josipgit <josipradictg@gmail.com>
2025-05-17 11:07:16 +0200

    Delete src directory

--- src/J1Dr1MainDateTime.java
+++
@@ -1,8 +0,0 @@
-import java.time.LocalDateTime;
-
-public class J1Dr1MainDateTime {
-    public static void main(String[] args) {
-        LocalDateTime dt = LocalDateTime.now();
-        System.out.println(dt);
-    }
-}
--- src/J27Test/Test.java
+++
@@ -1,7 +0,0 @@
-package J27Test;
-
-public class Test {
-    public static void main(String[] args) {
-
-    }
-}
